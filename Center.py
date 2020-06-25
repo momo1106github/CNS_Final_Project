c06dda2cccc4a2626b6ae76f99627bb7656b5f88
momo1106 <nomofika@gmail.com>
2020-06-25 10:05:43 +0800

    add GSJ for kRR

--- Center.py
+++ Center.py
@@ -1,7 +1,8 @@
 import argparse
 import random
 import logging
-from math import exp
+from sklearn.metrics import confusion_matrix
+from math import exp, log
 from zipf import zipf
 from kRR import kRR
 from MGA import MGA
@@ -65,6 +66,7 @@ class Center:
 		self.round = round
 		self.n = good_guys.n
 		self.m = bad_guys.m
+		self.d = good_guys.d
 		self.good_guys = good_guys
 		self.bad_guys = bad_guys
 		self.true_f_before_attack = []
@@ -73,11 +75,87 @@ class Center:
 		self.diff_f_vs_estimated_f_before_attack = []
 		self.diff_f_vs_estimated_f_before_attack_norm = []
 	
+	def count_frequency(self, P):
+		f = [0] * self.d
+		for i in P:
+			f[i] += 1
+		return f
+
+	def D_KL(self, P, Q):
+		sum = 0
+		for a in range(self.d):
+			try:
+				sum += P[a] * log(P[a] / Q[a])
+			except:
+				sum += 0
+		return sum
+
+	def GJS(self, P, Q, alpha):
+		Q_ = [(P[i] + alpha*Q[i]) / (1 + alpha) for i in range(self.d)]
+		# print ('== in GJS ==')
+		# print ('P :', P)
+		# print ('Q :', Q)
+		# print ('Q_ :', Q_)
+		return self.D_KL(P, Q_) + alpha * self.D_KL(Q, Q_)
+
 	def Algorithm(self):
 		"""
 		The function of our Algorithm.
 		"""
-		pass
+		N = 300
+		P = [self.good_guys.perturbedItems[i] for i in range(N)]
+		Q = [self.bad_guys.Items[i] for i in range(N)]
+
+		# print ('P :', P)
+		# print ('Q :', Q)
+
+		P_hat = self.count_frequency(P)
+		Q_hat = self.count_frequency(Q)
+
+		print ('P_hat :', P_hat)
+		print ('Q_hat :', Q_hat)
+
+		threshold = 10
+
+		numTest = 1000
+		n = 300
+
+		y_true = [0] * (int(numTest*0.9)) + [1] * (int(numTest*0.1)) # 0->good, 1->bad
+		y_predict = []
+
+		mx_good = 0
+		mn_good = 9999999999999
+
+		for t in range(int(numTest * 0.9)): # test good guys
+			rand_index = [i for i in range(self.n)]
+			random.shuffle(rand_index)
+			rand_index = rand_index[:n]
+
+			Pi_xn = self.count_frequency([self.good_guys.perturbedItems[i] for i in rand_index])
+			y_predict.append(0 if self.GJS(Pi_xn, Q_hat, N / n) >= threshold else 1)
+			mx_good = max(mx_good, self.GJS(Pi_xn, Q_hat, N / n))
+			mn_good = min(mn_good, self.GJS(Pi_xn, Q_hat, N / n))
+
+		print ('mx_good :', mx_good)
+		print ('mn_good :', mn_good)
+
+		mx_bad = 0
+		mn_bad = 9999999999999
+
+		for t in range(int(numTest * 0.1)): # test bad guys
+			rand_index = [i for i in range(self.m)]
+			random.shuffle(rand_index)
+			rand_index = rand_index[:n]
+
+			Pi_xn = self.count_frequency([self.bad_guys.Items[i] for i in rand_index])
+			y_predict.append(0 if self.GJS(Pi_xn, Q_hat, N / n) >= threshold else 1)
+			mx_bad = max(mx_bad, self.GJS(Pi_xn, Q_hat, N / n))
+			mn_bad = min(mn_bad, self.GJS(Pi_xn, Q_hat, N / n))
+
+		print ('mx_bad :', mx_bad)
+		print ('mn_bad :', mn_bad)
+
+		print ("confusion_matrix:\n",confusion_matrix(y_true, y_predict))
 
 	def validation(self):
 		"""
@@ -112,12 +190,14 @@ class Center:
 			self.bad_guys.getItems()
 			print (f"bad_guys: {self.bad_guys.Items}")
 
-			# our algorithm
-			self.Algorithm()
+			# GSJ for kRR
+			print (type(self.good_guys.protocol))
+			if type(self.good_guys.protocol) == kRR:
+				self.Algorithm()
 			
-			self.validation()
+			#self.validation()
 			#logging.info(f'diff_f_vs_estimated_f: {self.diff_f_vs_estimated_f}')
-			print ('diff_f_vs_estimated_f_norm:', [round(i, 3) for i in self.diff_f_vs_estimated_f_before_attack_norm])
+			#print ('diff_f_vs_estimated_f_norm:', [round(i, 3) for i in self.diff_f_vs_estimated_f_before_attack_norm])
 
 
 def parse_arguments():
